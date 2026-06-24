0b515e296dd314d41018e228ab56a60323b3b2ac
Alon Kabilo <kabilofamily@hotmail.com>
2026-06-24 22:22:51 +0300

    poc

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def default_user_name():
     return f"User {len(st.session_state.sessions) + 1}"
 
 
-def add_user(name):
+def add_conversation(name):
     name = name.strip() or default_user_name()
     if name in st.session_state.sessions:
         suffix = 2
@@ -48,14 +48,14 @@ def add_user(name):
 
 
 with st.sidebar:
-    with st.form("add_user"):
+    with st.form("add_conversation"):
         user_name = st.text_input("User name", value=default_user_name())
-        if st.form_submit_button("Add user"):
-            add_user(user_name)
+        if st.form_submit_button("Add conversation"):
+            add_conversation(user_name)
             st.rerun()
 
 if not st.session_state.sessions:
-    st.info("Add a user from the sidebar to start.")
+    st.info("Add a conversation from the sidebar to start.")
 else:
     tabs = st.tabs(list(st.session_state.sessions.keys()))
     for label, tab in zip(st.session_state.sessions, tabs):
